ea41929486ea8ea4d84df1ea886f4ba55b0cc636
nicolevaleriee <nicolevaleriee1@gmail.com>
2025-11-10 13:46:34 +0700

    book class, user class, librarian class, member class (initialization, constructor)

--- Lib.java
+++ Lib.java
@@ -4,8 +4,6 @@ enum Genre {
     ROMANCE,
     SELF_HELP,
     BIOGRAPHY, 
-    SCIENCE_FICTION,
-    MYSTERY
 }
 
 enum BookStatus {
@@ -13,3 +11,43 @@ enum BookStatus {
     BORROWED
 }
 
+class Book{
+    String bookID;
+    String title;
+    String author;
+    Genre genre;
+    double price;
+    BookStatus bookStatus;
+    int quantity
+
+    public Book(String bookID, String title, String author, Genre genre, double price, BookStatus bookStatus)  {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.price = price;
+        this.bookStatus = bookStatus;
+        this.quantity = quantity;
+    }
+}
+
+class user{
+    String name;
+    String userName;
+}
+
+class librarian extends user{
+    String librarianID;
+    public librararian(String name, String userName, String librarianID){
+        super(name, userName);
+        this.librarianID = librarianID;
+    }   
+}
+
+class member extends User{
+    String memberID;
+    public member(String name, String userName, String memberID){
+        super(name, userName);
+        this.memberID = memberID;
+    }
+}
